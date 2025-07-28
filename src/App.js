f203c02a41ce06f78b0df12090f5caa9ed053bf0
alexis <alexisalfaro505@gmail.com>
2025-07-28 17:24:04 -0600

    Create all the interface of the calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const Calculator = () => {
+
+  const [input, setInput] = ([]);
+  const [result, setResult] = ([]);
+
+  const handleClick = (key) => {
+
+  };
+
+  return(
+    <div className='calculator'>
+      <h2>React Calculator</h2>
+
+      <div className='screen'>
+        <input type='text' readOnly value={input} placeholder='0'></input>
+        <input type='text' readOnly value={result} placeholder='Result'></input>
+      </div>
+
+      <div className='button-row'>
+        {['7', "8", "9", "/"].map((item) => (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+
+        {['4', "5", "6", "*"].map((item) => (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+
+        {['1', "2", "3", "-"].map((item) => (
+          <button key={item} onClick={() => handleClick(item)}>
+            {item}
+          </button>
+        ))}
+
+        <button onClick={handleClick("0")}>0</button>
+        <button onClick={handleClick("+")}>+</button>
+        <button className='equals' onClick={handleClick("=")}>=</button>
+        <button className='clear' onClick={handleClick("Clear")}>=</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default Calculator;
